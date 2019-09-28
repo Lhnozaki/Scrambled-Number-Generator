34c125bd7bd2af414ebe92d2840560d680dc1b5c
Lewis Nozaki <lhnozaki@gmail.com>
2019-09-28 09:41:08 -1000

    passed tests

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 module.exports = function(n) {
   // do work here
+  let arr = [];
+
+  for (i = 1; i < n; i++) {
+    if (i % 2 === 0) {
+      arr.push(i);
+    } else if (i % 2 !== 0) {
+      arr.unshift(i);
+    }
+  }
+
+  arr.unshift(0);
+  return arr;
 };
